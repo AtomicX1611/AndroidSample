95554c0e3e528f70bbc785b64f53426fc072bfa3
AtomicX1611 <146544040+AtomicX1611@users.noreply.github.com>
2024-01-14 11:49:55 +0530

    Vesrion 3.0

--- app/src/main/java/com/example/androidsample/MainActivity.java
+++ app/src/main/java/com/example/androidsample/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Releaing version 2.0
+        // Releaing version 3.0
     }
 }
